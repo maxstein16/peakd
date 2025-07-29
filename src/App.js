53f327058f83788505803223ff0837ad4582ca70
Max Stein <mhs8558@rit.edu>
2025-07-29 17:48:03 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
+import { FaInstagram, FaTiktok } from 'react-icons/fa';
 import Typewriter from 'typewriter-effect';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter
-            .typeString('peakd')
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString('coming soon')
-            .pauseFor(2000)
-            .deleteAll()
-            .start();
-        }}
-        options={{
-          loop: true,
-          delay: 100,
-          deleteSpeed: 50,
-        }}
-      />
+      {/* Background layer with mountain and stars */}
+      <div className="background-layer"></div>
+      
+      {/* Text layer */}
+      <div className="text-container">
+        <h1 className="peakd-text">
+          <Typewriter
+            options={{
+              strings: ['peakd', '<span class="coming-soon">coming soon</span>'],
+              autoStart: true,
+              loop: true,
+              delay: 200,
+              cursor: '',
+              deleteSpeed: 50
+            }}
+          />
+        </h1>
+      </div>
+      
+      {/* Social media logos */}
+      <div className="social-logos">
+        <a href="https://www.instagram.com/peakd.app/" target="_blank" rel="noopener noreferrer" className="social-logo">
+          <FaInstagram />
+        </a>
+        <a href="https://www.tiktok.com/@peakd.app" target="_blank" rel="noopener noreferrer" className="social-logo">
+          <FaTiktok />
+        </a>
+      </div>
     </div>
   );
 }
